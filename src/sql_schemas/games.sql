05bb536ea5181a62fbde41605f1668dfdd5e04bc
Colin Brown <kolivbrauv@gmail.com>
2018-10-22 21:15:31 -0700

    Updated games schema
    Made game_id INTEGER, renamed player score columns

--- src/sql_schemas/games.sql
+++ src/sql_schemas/games.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `games` (
-	`game_id`	NUMERIC,
+	`game_id`	INTEGER UNIQUE,
 	`player1`	INTEGER,
 	`player2`	INTEGER,
-	`player1score`	INTEGER,
-	`player2score`	INTEGER,
+	`player1_score`	INTEGER,
+	`player2_score`	INTEGER,
 	FOREIGN KEY(`player1`) REFERENCES `players`(`player_id`),
 	FOREIGN KEY(`player2`) REFERENCES `players`(`player_id`),
 	PRIMARY KEY(`game_id`)
